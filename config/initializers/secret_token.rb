108a44f07122cfc0a37ea90789cfa6fefb07bbc4
legendaryfox <legendaryfox@gmail.com>
2011-11-05 22:08:38 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tcproj::Application.config.secret_token = 'e72df18e106b21c0a5870b7e043e312207b6f84a275724067ece7d05f54e25ecbbb84d8ff2dbd6d6966d7ab875e56c195b7e53e61095332930b712ac7eb9da05'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tcproj::Application.config.session_store :cookie_store, :key => '_tcproj_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Tcproj::Application.config.session_store :active_record_store
