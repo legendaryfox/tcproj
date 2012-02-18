9565efa314c3b501ea96115fbab1c54a52d55f32
Josiah <legendaryfox@gmail.com>
2012-02-18 16:14:42 +0800

    Fixed bug where single unverified point crashes app

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -48,26 +48,35 @@ class PagesController < ApplicationController
       # A category is provided
       unjoined_cbos.each do |unjoined_cbo|
         if unjoined_cbo.cboprofile.category_id == @selected_category.to_i
-          @unjoined_cboprofiles.push(unjoined_cbo.cboprofile)
+          if !unjoined_cbo.cboprofile.latitude.blank? && !unjoined_cbo.cboprofile.longitude.blank?
+            @unjoined_cboprofiles.push(unjoined_cbo.cboprofile)
+            puts "CBO PROFILE IS #{unjoined_cbo.cboprofile}"
+            
+          end
+          
         end
       end
       
       joined_cbos.each do |joined_cbo|
         if joined_cbo.cboprofile.category_id == @selected_category.to_i
-          @joined_cboprofiles.push(joined_cbo.cboprofile)
+          if !joined_cbo.cboprofile.latitude.blank? && !joined_cbo.cboprofile.longitude.blank?
+            @joined_cboprofiles.push(joined_cbo.cboprofile)
+            puts "CBO PROFILE IS #{unjoined_cbo.cboprofile}"
+            
+          end
+          
         end
       end
      
       @category_name = Category.find_by_id(@selected_category.to_i).name
       
-      
-      #@unjoined_cboprofiles = unjoined_cbos.collect {|unjoined_cbo| if (unjoined_cbo.cboprofile.category_id == @selected_category) unjoined_cbo.cboprofile end }
-  	  #@joined_cboprofiles = joined_cbos.collect {|joined_cbo| joined_cbo.cboprofile }
     else
       # No category provided - no filtering
+
       @unjoined_cboprofiles = unjoined_cbos.collect {|unjoined_cbo| unjoined_cbo.cboprofile }
   	  @joined_cboprofiles = joined_cbos.collect {|joined_cbo| joined_cbo.cboprofile }
-  	  
+
+ 	  
   	  @category_name = 'All Categories'
   	  
     end
