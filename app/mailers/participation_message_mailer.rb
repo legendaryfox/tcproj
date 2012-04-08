c2e8247b25d77e3db7fac857761aa720ea1492f8
Josiah <legendaryfox@gmail.com>
2012-04-08 19:38:08 +0800

    Small typo in Participation Message Mailer

--- app/mailers/participation_message_mailer.rb
+++ app/mailers/participation_message_mailer.rb
@@ -16,7 +16,7 @@ class ParticipationMessageMailer < ActionMailer::Base
     @cbo = participation_message.cbo
     @user = participation_message.user
     @participation_message = participation_message
-    @url = "http://#{request.host_with_por}" + participation_path(participation_message.participation) #request.url #request.host_with_port + participation_path(participation_message.participation)
+    @url = "http://#{request.host_with_port}" + participation_path(participation_message.participation) #request.url #request.host_with_port + participation_path(participation_message.participation)
     
   
     mail(:to => @cbo.email, :subject => "New message from #{@user.userprofile.name}!")
