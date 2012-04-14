357a5d98ae913448210b870458ad1429932d1e87
Josiah <legendaryfox@gmail.com>
2012-04-14 12:51:14 +0800

    Typo in controller. Thanks Mike

--- app/controllers/cbo_community_memberships_controller.rb
+++ app/controllers/cbo_community_memberships_controller.rb
@@ -18,7 +18,7 @@ class CboCommunityMembershipsController < ApplicationController
     if current_cbo.leave_community!(@community)
       redirect_to @community, :flash => { :success => "You ahve successfully left this Community." }
     else
-      reidrect_to @community, :flash => { :error => "There was an error in processing your request." }
+      redirect_to @community, :flash => { :error => "There was an error in processing your request." }
     end
   end
 
